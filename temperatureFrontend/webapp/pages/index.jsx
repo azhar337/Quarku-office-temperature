4a0b4382440ea0d5fc1ebf825801992aa8d5f6f2
azhar337 <muhdazharali98@gmail.com>
2022-04-28 15:27:16 +0800

    FRONTEND: Chakra testing on register and home page

--- temperatureFrontend/webapp/pages/index.jsx
+++ temperatureFrontend/webapp/pages/index.jsx
@@ -2,53 +2,76 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
-import { ColorModeScript } from '@chakra-ui/react'
+import { Button, ButtonGroup, Center, Heading, Flex } from '@chakra-ui/react'
 import theme from '../styles/theme'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Office Temperature</title>
-        <meta name="description" content="Its fun and game until you forget your sweater and get fever" />
-        <link rel="icon" href="/fav.PNG" />
-      </Head>
-      <ColorModeScript />
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          What my office <b>temperature?</b>
-        </h1>
-
-        <p className={styles.description}>
-          Get your office temperature analysis
-        </p>
+    // <div className={styles.container}>
+    //   <Head>
+    //     <title>Office Temperature</title>
+    //     <meta name="description" content="Its fun and game until you forget your sweater and get fever" />
+    //     <link rel="icon" href="/fav.PNG" />
+    //   </Head>
       
-        <div className={styles.grid}>
-          <Link href="/register">
-            <a className={styles.card}>
-              <h2>Register &rarr;</h2>
-        
-            </a>
-          </Link>
+    //   <main className={styles.main}>
+    //     <h1 className={styles.title}>
+    //       What my office <b>temperature?</b>
+    //     </h1>
 
-          <Link href="/login">
-            <a className={styles.card}>
-              <h2>Login &rarr;</h2>
-            </a>
-          </Link>
-        </div>
-      </main>
+    //     <p className={styles.description}>
+    //       Get your office temperature analysis
+    //     </p>
+      
+    //     <div className={styles.grid}>
+    //       <Link href="/register">
+    //         <Button colorScheme='blue' size='lg' m='10px'>Register</Button>
+    //       </Link>
+
+    //       <Link href="/login">
+    //           <Button colorScheme='teal' size='lg' m='10px'>Login</Button>
+    //       </Link>
+    //     </div>
+    //   </main>
      
-      <footer className={styles.footer}>
-        <a
-          href="https://www.google.com/search?q=air+conditioner&rlz=1C1CHBF_enMY996MY996&oq=Aircondi&aqs=chrome.1.69i57j0i10i512l5j0i10i457i512j0i402j0i10i512l2.9213j0j7&sourceid=chrome&ie=UTF-8"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by air cond
+    //   <footer className={styles.footer}>
+    //     <a
+    //       href="https://www.google.com/search?q=air+conditioner&rlz=1C1CHBF_enMY996MY996&oq=Aircondi&aqs=chrome.1.69i57j0i10i512l5j0i10i457i512j0i402j0i10i512l2.9213j0j7&sourceid=chrome&ie=UTF-8"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Powered by air cond
  
-        </a>
-      </footer>
-    </div>
+    //     </a>
+    //   </footer>
+    // </div>
+
+    <Center>
+      <Flex
+        flexDir="column"  
+        marginTop="10em"
+      >
+      <Heading as='h1' m='30px'>My office <b>temperature</b></Heading>
+       
+        <Flex 
+          flexDir="row"
+          w='100%'
+          m='30px'
+        >
+          <Center>
+              <Link href="/register">
+              <Button colorScheme='blue' size='lg' m='10px'>Register</Button>
+            </Link>
+
+            <Link href="/login">
+                <Button colorScheme='teal' size='lg' m='10px'>Login</Button>
+            </Link>
+           </Center>
+  
+           </Flex>
+          
+      </Flex>
+
+    </Center>
   )
 }
--- temperatureFrontend/webapp/pages/register/index.jsx
+++ temperatureFrontend/webapp/pages/register/index.jsx
@@ -3,6 +3,7 @@ import { sha256 } from 'js-sha256';
 import axios from '../../base-axios'
 import { useState, useEffect } from "react"
 import Router from 'next/router'
+import { Center, Flex, Heading, Input } from '@chakra-ui/react'
 
 
 
@@ -65,19 +66,42 @@ export default function Register() {
 
     return (
         
-        <div className={styles.container}>
-            <h1 className={styles.title}> Register </h1>
-            <div className={styles.description}>            
-                    <form  method="POST" onSubmit={register}>
-                        <label for="email">Email:</label>
-                        <input type="email" id="email"  name="email"/>
-                        <label for="password">Password:</label>
-                        <input type="password" id="password" name="password" ></input>
-                        <label for="confirmPassword"> Confirm Password:</label>
-                        <input type="password" id="confirmPassword" name="confirmPassword" ></input><br></br>
-                        <input type="submit" value="Register"></input>
-                    </form>
-            </div>
-        </div>
+        // <div className={styles.container}>
+        //     <h1 className={styles.title}> Register </h1>
+        //     <div className={styles.description}>            
+        //             <form  method="POST" onSubmit={register}>
+        //                 <label for="email">Email:</label>
+        //                 <input type="email" id="email"  name="email"/>
+        //                 <label for="password">Password:</label>
+        //                 <input type="password" id="password" name="password" ></input>
+        //                 <label for="confirmPassword"> Confirm Password:</label>
+        //                 <input type="password" id="confirmPassword" name="confirmPassword" ></input><br></br>
+        //                 <input type="submit" value="Register"></input>
+        //             </form>
+        //     </div>
+        // </div>
+        <Center >
+            <Flex 
+                flexDir="column" 
+                boxShadow="0 4px 12px 0 rgba(0, 0, 0, 0.05)" 
+                marginTop="10em"
+                justifyContent="space-between"
+                padding='30px'
+                width='30em'
+                bg='#FED7E2'
+                borderRadius='30px'
+
+            >   <Center>
+                <Heading  mb='50px' color='#2D3748'>Register Here</Heading>
+                </Center>
+                <form  method="POST" onSubmit={register}>
+                    <Input placeholder='email' size='lg' id='email' type="email" m='10px' bg='white'/>
+                    <Input placeholder='password' size='lg' id='password' type="password" m='10px'  bg='white'/>
+                    <Input placeholder='confirm password' id='confirmPassword' size='lg' type="password" m='10px'  bg='white'/>
+                    <Input placeholder='confirm password' size='lg' type="submit" m='10px' mt='30px' color='white'  bg='black'  _hover={{bg:'red',cursor: 'pointer'}}/>
+
+                </form>
+            </Flex>
+        </Center>
     )
 }
